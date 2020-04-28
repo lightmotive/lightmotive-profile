e2130bee6d0ab9b2dd4db5f38d4f5fc2d5e350ea
alight1 <andrelight.al@gmail.com>
2020-04-27 18:07:14 -0600

    Remove freeCodeCamp test utility

--- src/index.js
+++ src/index.js
@@ -8,10 +8,3 @@ faviconLink.href = faviconPath;
 document.head.appendChild(faviconLink);
 
 document.getElementById("body").innerHTML += bodyContent;
-
-//Append freeCodeCamp test utility (does not work when added directly to HTML)
-localStorage.setItem("project_selector", "portfolio");
-const fccScript = document.createElement("script");
-fccScript.src =
-  "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
-document.head.appendChild(fccScript);
