b1160b9d0dae0eb6384b2ad255f0ca8bebe488c5
alight1 <andrelight.al@gmail.com>
2020-05-04 10:27:10 -0600

    Consolidate styles back into one file
    * VS Code doesn't support importing variables.
    * No extensions exist to support the same.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 require("../src/style.css");
-require("../src/style-site-nav.css");
-require("../src/style-projects.css");
-require("../src/style-contact.css");
 import bodyContent from "../src/body-content.html";
 import faviconPath from "../src/assets/logos/favicon_symbol.png";
 
