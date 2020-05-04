06670a416af8af220e8e33d0b7f948e8892f9b1d
alight1 <andrelight.al@gmail.com>
2020-05-04 10:07:09 -0600

    Reorganize styles
    * Separate files for style groups

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 require("../src/style.css");
+require("../src/style-site-nav.css");
+require("../src/style-projects.css");
+require("../src/style-contact.css");
 import bodyContent from "../src/body-content.html";
 import faviconPath from "../src/assets/logos/favicon_symbol.png";
 
